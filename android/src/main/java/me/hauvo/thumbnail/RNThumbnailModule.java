05eb1e55dfb640e0082922272207d2549d61060b
liubin <liubin20170101>
2019-03-19 11:54:02 +0800

    配置文件

--- android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
+++ android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
@@ -1,27 +1,23 @@
 
 package me.hauvo.thumbnail;
 
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
-import android.media.ThumbnailUtils;
-import android.provider.MediaStore;
-import android.net.Uri;
-import android.provider.MediaStore.Video.Thumbnails;
-import android.graphics.Bitmap;
-import android.os.Environment;
-import android.util.Log;
-import android.media.MediaMetadataRetriever;
-import 	android.graphics.Matrix;
 
-import java.util.UUID;
 import java.io.File;
-import java.io.OutputStream;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 
 public class RNThumbnailModule extends ReactContextBaseJavaModule {

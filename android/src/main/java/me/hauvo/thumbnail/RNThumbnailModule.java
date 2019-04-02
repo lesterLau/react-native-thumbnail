97c59c1a6c2905dd4d22d0c251ac79ee634137ea
liubin <liubin20170101>
2019-04-02 19:23:11 +0800

    bug修改

--- android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
+++ android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
@@ -22,59 +22,61 @@ import java.util.UUID;
 
 public class RNThumbnailModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNThumbnailModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNThumbnail";
-  }
-
-  @ReactMethod
-  public void get(String filePath, Promise promise) {
-    filePath = filePath.replace("file://","");
-    filePath = QrcodeUtil.getImagePath(reactContext, Uri.parse(filePath), null);
-    MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-    retriever.setDataSource(filePath);
-    Bitmap image = retriever.getFrameAtTime(1000000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
-
-    String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/thumb";
-
-    try {
-      File dir = new File(fullPath);
-      if (!dir.exists()) {
-        dir.mkdirs();
-      }
-
-      OutputStream fOut = null;
-      // String fileName = "thumb-" + UUID.randomUUID().toString() + ".jpeg";
-      String fileName = "thumb-" + UUID.randomUUID().toString() + ".jpeg";
-      File file = new File(fullPath, fileName);
-      file.createNewFile();
-      fOut = new FileOutputStream(file);
-
-      // 100 means no compression, the lower you go, the stronger the compression
-      image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
-      fOut.flush();
-      fOut.close();
-
-      // MediaStore.Images.Media.insertImage(reactContext.getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
-
-      WritableMap map = Arguments.createMap();
-
-      map.putString("path", "file://" + fullPath + '/' + fileName);
-      map.putDouble("width", image.getWidth());
-      map.putDouble("height", image.getHeight());
-
-      promise.resolve(map);
-
-    } catch (Exception e) {
-      Log.e("E_RNThumnail_ERROR", e.getMessage());
-      promise.reject("E_RNThumnail_ERROR", e);
+    public RNThumbnailModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
     }
-  }
+
+    @Override
+    public String getName() {
+        return "RNThumbnail";
+    }
+
+    @ReactMethod
+    public void get(String filePath, Promise promise) {
+        if (!filePath.contains("file://")) {
+            filePath = QrcodeUtil.getImagePath(reactContext, Uri.parse(filePath), null);
+        }
+        filePath = filePath.replace("file://", "");
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(filePath);
+        Bitmap image = retriever.getFrameAtTime(1000000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+
+        String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/thumb";
+
+        try {
+            File dir = new File(fullPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            OutputStream fOut = null;
+            // String fileName = "thumb-" + UUID.randomUUID().toString() + ".jpeg";
+            String fileName = "thumb-" + UUID.randomUUID().toString() + ".jpeg";
+            File file = new File(fullPath, fileName);
+            file.createNewFile();
+            fOut = new FileOutputStream(file);
+
+            // 100 means no compression, the lower you go, the stronger the compression
+            image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+
+            // MediaStore.Images.Media.insertImage(reactContext.getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
+
+            WritableMap map = Arguments.createMap();
+
+            map.putString("path", "file://" + fullPath + '/' + fileName);
+            map.putDouble("width", image.getWidth());
+            map.putDouble("height", image.getHeight());
+
+            promise.resolve(map);
+
+        } catch (Exception e) {
+            Log.e("E_RNThumnail_ERROR", e.getMessage());
+            promise.reject("E_RNThumnail_ERROR", e);
+        }
+    }
 }
